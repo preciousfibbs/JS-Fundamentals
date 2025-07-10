ff0be673ab3d3d5f692bd9354c301324afc0aa3c
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-07-10 21:43:54 +0100

    Add 1-multi_languages.js script

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+// This code declares three variables `line1`, `line2`, and `line3` and assigns them strings representing different programming languages.
+// It then prints each of these variables to the console.
